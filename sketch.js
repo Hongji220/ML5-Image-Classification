d56d97eae4e54369ec80291ce4421d450b6ae22c
Hon <hongji220@gmail.com>
2018-08-02 16:02:23 +0100

    Initialization
    Initialized index.html and Sketch,js and others.

--- sketch.js
+++ sketch.js
@@ -1,1 +1,16 @@
-// JavaScript Document
+let mobilenet;
+
+function modelReady() {
+	console.log("Model is ready!");
+}
+
+
+function setup() {
+	createCanvas(640,480)
+	background(0);
+	mobilenet = ml5.imageClassifier('MobileNet', modelReady() );
+}
+
+function draw() {
+	
+}
