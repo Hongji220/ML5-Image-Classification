052536578d2653e7504c56d39eec9619c87cac0d
Hon <hongji220@gmail.com>
2018-08-03 16:03:39 +0100

    Classification
    Images and classifier created.

--- sketch.js
+++ sketch.js
@@ -1,32 +1,33 @@
-classifier.predict(image, function(err, results) {
-        result.innerText = results[0].className;
-        probability.innerText = results[0].probability.toFixed(4);
-      });classifier.predict(image, function(err, results) {
-        result.innerText = results[0].className;
-        probability.innerText = results[0].probability.toFixed(4);
-      });classifier.predict(image, function(err, results) {
-        result.innerText = results[0].className;
-        probability.innerText = results[0].probability.toFixed(4);
-      });// The image we want to classify
-      const image = document.getElementById('image');
-      // The result tag in the HTML
-      const result = document.getElementById('result');
-      // The probability tag in the HTML
-      const probability = document.getElementById('probability');// The image we want to classify
-      const image = document.getElementById('image');
-      // The result tag in the HTML
-      const result = document.getElementById('result');
-      // The probability tag in the HTML
-      const probability = document.getElementById('probability');// The image we want to classify
-      const image = document.getElementById('image');
-      // The result tag in the HTML
-      const result = document.getElementById('result');
-      // The probability tag in the HTML
-      const probability = document.getElementById('probability');const mobilenet = ml5.imageClassifer('MobileNet', modelLoaded);
+let mobilenet;
+let puffin;
 
-function modelLoaded() {
-	console.log('Model Loaded!');
-	
+function modelReady() {
+  console.log('Model is ready!!!');
+  mobilenet.predict(puffin, gotResults);
 }
 
-mobilenet.predict(document.getElementById('image'), function(error, results) {console.log(results)});
+function gotResults(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+    let label = results[0].className;
+    let prob = results[0].probability.toFixed(3)*100 + "%";
+    select("#classname").html(label);
+    select("#prob").html(prob);
+  }
+}
+
+function imageReady() {
+  image(puffin, 0, 0, width, height);
+}
+
+function setup() {
+  createCanvas(640, 480);
+  puffin = createImg('images/dog.jpg', imageReady);
+  puffin.hide();
+  background(0);
+  mobilenet = ml5.imageClassifier('MobileNet', modelReady);
+  createP().id("classname");
+  createP().id("prob");
+}
