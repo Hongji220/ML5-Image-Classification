abeda080112b7a2c85184c3bd93860b6a365073d
Hon <hongji220@gmail.com>
2018-08-05 22:08:18 +0100

    Some Changes
    Simple Changes

--- Webcam Demo/sketch.js
+++ Webcam Demo/sketch.js
@@ -2,6 +2,7 @@ let mobilenet;
 
 let video;
 
+let label = '';
 
 function modelReady() {
   console.log('Model is ready!!!');
@@ -14,11 +15,14 @@ function gotResults(error, results) {
   if (error) {
     console.error(error);
   } else {
-    console.log(results);
+    //console.log(results);
     let label = results[0].className;
     let prob = results[0].probability.toFixed(3)*100 + "%";
-    select("#classname").html(label);
-    select("#prob").html(prob);
+    
+
+    createP(label);
+    createP(prob);
+    mobilenet.predict(gotResults);
 
   }
 
@@ -28,14 +32,18 @@ function gotResults(error, results) {
 
 
 function setup() {
-  createCanvas(640,480);
+  createCanvas(640,550);
   video = createCapture(VIDEO);
   video.hide(); 
-  mobilenet = ml5.imageClassifier('MobileNet', video ,modelReady);
+  background(0);
+  mobilenet = ml5.imageClassifier('MobileNet', video , modelReady);
   createP().id("classname");
   createP().id("prob");
 }
 
 function draw() {
   image(video, 0 , 0);
+  fill(0);
+  textSize(25);
+  text(label, 10 , height-20);
 }
