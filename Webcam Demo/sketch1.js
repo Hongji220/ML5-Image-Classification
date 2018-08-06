d89252c4569aee11b65ff97ef81275ffce13ec52
Hon <hongji220@gmail.com>
2018-08-06 14:54:36 +0100

    Changed Directory Structure
    Directry Structure.

---
+++ Webcam Demo/sketch1.js
@@ -0,0 +1,53 @@
+let mobilenet;
+
+let video;
+
+let label = '';
+
+let prob = '';
+
+function modelReady() {
+  console.log('Model is ready!!!');
+  mobilenet.predict(gotResults);
+}
+
+
+
+function gotResults(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    //console.log(results);
+    label = results[0].className;
+    prob = results[0].probability.toFixed(3)*100 + "%";
+    
+/*
+    createP(label);
+    createP(prob);*/
+    mobilenet.predict(gotResults);
+
+  }
+
+}
+
+
+
+
+function setup() {
+  createCanvas(640,550);
+  video = createCapture(VIDEO , function() {console.log("Video Ready!")});
+  video.hide(); 
+  background(0);
+  mobilenet = ml5.imageClassifier('MobileNet', video , modelReady);
+/*  createP().id("classname");
+  createP().id("prob");*/
+}
+
+function draw() {
+  background(0);
+  image(video, 0 , 0);
+  fill(255);
+  textSize(32);
+  text(label, 10, height - 20);
+  text(prob, 10, 30);
+}
