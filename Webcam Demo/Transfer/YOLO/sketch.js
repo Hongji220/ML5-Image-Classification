8560a09aeedc711046fb257b9ccaeee8db151be2
Hon <hongji220@gmail.com>
2018-08-08 15:03:01 +0100

    C
    c

--- Webcam Demo/Transfer/YOLO/sketch.js
+++ Webcam Demo/Transfer/YOLO/sketch.js
@@ -4,7 +4,7 @@ let status;
 let objects = [];
 
 function setup() {
-  createCanvas(640, 480);
+  createCanvas(720, 576);
   video = createCapture(VIDEO);
 
   // Create a YOLO method
@@ -16,7 +16,7 @@ function setup() {
 }
 
 function draw() {
-  image(video, 0, 0, 640, 480);
+  image(video, 0, 0, 720, 576);
   for (let i = 0; i < objects.length; i++) {
     noStroke();
     fill(0, 255, 0);
